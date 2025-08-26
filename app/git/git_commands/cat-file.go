5345fa035ccf898c016e006e5e1b4b8bf73e84e5
Aritra640 <chatterjeearitra15@gmail.com>
2025-08-26 06:24:00 +0530

    minor changes

--- app/git/git_commands/cat-file.go
+++ app/git/git_commands/cat-file.go
@@ -1,1 +1,7 @@
 package gitcommands
+
+//git cat-file -p hash
+func gitcatfile(flag string, commitSHA string) {
+	
+	
+}
--- app/git/git_commands/client.go
+++
@@ -1,5 +0,0 @@
-package gitcommands 
-
-type GitClient interface {
-	Execute()
-}
