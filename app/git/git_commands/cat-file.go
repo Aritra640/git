dc2a7c59dd5b3b0de44ef8255838887dd22df209
Aritra640 <chatterjeearitra15@gmail.com>
2025-08-27 23:10:22 +0530

    minor changes

--- app/git/git_commands/cat-file.go
+++ app/git/git_commands/cat-file.go
@@ -3,5 +3,11 @@ package gitcommands
 //git cat-file -p hash
 func gitcatfile(flag string, commitSHA string) {
 	
-	
+	switch flag {
+	case "-p" :
+		folder := commitSHA[:2]
+		file := commitSHA[2:]
+
+		completePath := 
+	}
 }
