99caf0548f41ec9ce364a89cec844e1813d430ce
Aritra640 <chatterjeearitra15@gmail.com>
2025-09-02 23:33:50 +0530

    stage ls-tree

--- app/git/git_commands/ls-tree.go
+++ app/git/git_commands/ls-tree.go
@@ -1,6 +1,10 @@
 package gitcommands
 
-import "os"
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
 
 type Git_ls_tree struct {
 	flag string
@@ -24,5 +28,19 @@ func (g *Git_ls_tree) Execute() {
 	folder := g.sha[:2]
 	file := g.sha[2:]
 
+	dir,err := os.Getwd(); if err != nil {
+		log.Fatal(err)
+	}
 
+	folderPath := filepath.Join(dir, ".git", "objects", folder)
+	if _,err = os.Stat(folderPath); err != nil {
+		log.Println("folder not found")
+		log.Fatal(err)
+	}
+
+	filePath := filepath.Join(folderPath , file) 
+	if _,err := os.Stat(filePath); err != nil {
+		log.Println("file not found")
+		log.Fatal(err)
+	}
 }
