2114a7af1c429c36f484e81898739f82b3b9a3ff
Aritra640 <chatterjeearitra15@gmail.com>
2025-09-02 23:51:00 +0530

    stage ls-tree

--- app/git/git_commands/ls-tree.go
+++ app/git/git_commands/ls-tree.go
@@ -1,6 +1,9 @@
 package gitcommands
 
 import (
+	"compress/zlib"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -22,25 +25,48 @@ func (g *Git_ls_tree) Init() {
 	}
 }
 
-
 func (g *Git_ls_tree) Execute() {
 
 	folder := g.sha[:2]
 	file := g.sha[2:]
 
-	dir,err := os.Getwd(); if err != nil {
+	dir, err := os.Getwd()
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	folderPath := filepath.Join(dir, ".git", "objects", folder)
-	if _,err = os.Stat(folderPath); err != nil {
+	if _, err = os.Stat(folderPath); err != nil {
 		log.Println("folder not found")
 		log.Fatal(err)
 	}
 
-	filePath := filepath.Join(folderPath , file) 
-	if _,err := os.Stat(filePath); err != nil {
+	filePath := filepath.Join(folderPath, file)
+	if _, err := os.Stat(filePath); err != nil {
 		log.Println("file not found")
 		log.Fatal(err)
 	}
+
+	fileContent, err := os.Open(filePath)
+	if err != nil {
+		log.Println("Error: cannot read file with hash ", g.sha)
+		log.Fatal(err)
+	}
+	defer fileContent.Close()
+
+	//create a zlib reader
+	reader, err := zlib.NewReader(fileContent)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+
+	decompressedData, err := io.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert to string and print
+	fmt.Println(string(decompressedData))
+
 }
